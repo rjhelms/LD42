fde6fdcca8e96a65615d211e1f4813344652ac32
Rob Hailman <robhailman@gmail.com>
2018-08-11 10:42:04 -0400

    Add phase in for phase barriers

---
+++ Assets/Scripts/PhaseBarrier.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PhaseBarrierState
+{
+    IN,
+    ACTIVE,
+    OUT
+}
+
+public class PhaseBarrier : MonoBehaviour {
+
+    public PhaseBarrierState State;
+    public float InTime;
+    public float OutTime;
+    public float FlashTime;
+
+    private float nextStateChangeTime;
+    private float nextFlashTime;
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rigidbody2D;
+
+	// Use this for initialization
+	void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.enabled = false;
+
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        nextStateChangeTime = Time.time + InTime;
+        nextFlashTime = Time.time + FlashTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (State != PhaseBarrierState.ACTIVE & Time.time > nextFlashTime)
+        {
+            nextFlashTime = Time.time + FlashTime;
+            spriteRenderer.enabled = !spriteRenderer.enabled;
+        }
+        switch (State)
+        {
+            case PhaseBarrierState.IN:
+                if (Time.time > nextStateChangeTime)
+                {
+                    spriteRenderer.enabled = true;
+                    rigidbody2D.simulated = true;
+                }
+                break;
+            case PhaseBarrierState.ACTIVE:
+                break;
+            case PhaseBarrierState.OUT:
+                break;
+        }
+	}
+}
