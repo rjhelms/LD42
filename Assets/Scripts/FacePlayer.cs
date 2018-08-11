d8056b5783a9d423d32804e3f5640d3375625e15
Rob Hailman <robhailman@gmail.com>
2018-08-10 21:29:35 -0400

    Add sprites that face player
    For that 2D feel

---
+++ Assets/Scripts/FacePlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacePlayer : MonoBehaviour {
+    private Transform player;
+
+    // Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(player);
+	}
+}
