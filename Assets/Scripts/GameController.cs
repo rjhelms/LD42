5ea126add86c322156153d3f06d4af3596eb1418
Rob Hailman <robhailman@gmail.com>
2018-08-10 22:53:11 -0400

    Begin 2D mockup

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    [Header("Resolution and Display")]
+    public Camera WorldCamera;
+    public int TargetX = 160;
+    public int TargetY = 200;
+    public Material RenderTexture;
+    private float pixelRatioAdjustment;
+
+    // Use this for initialization
+    void Start () {
+        pixelRatioAdjustment = (float)TargetX / (float)TargetY;
+        if (pixelRatioAdjustment <= 1)
+        {
+            RenderTexture.mainTextureScale = new Vector2(pixelRatioAdjustment, 1);
+            RenderTexture.mainTextureOffset = new Vector2((1 - pixelRatioAdjustment) / 2, 0);
+            WorldCamera.orthographicSize = TargetY / 2;
+        }
+        else
+        {
+            pixelRatioAdjustment = 1f / pixelRatioAdjustment;
+            RenderTexture.mainTextureScale = new Vector2(1, pixelRatioAdjustment);
+            RenderTexture.mainTextureOffset = new Vector2(0, (1 - pixelRatioAdjustment) / 2);
+            WorldCamera.orthographicSize = TargetX / 2;
+        }        pixelRatioAdjustment = (float)TargetX / (float)TargetY;
+        if (pixelRatioAdjustment <= 1)
+        {
+            RenderTexture.mainTextureScale = new Vector2(pixelRatioAdjustment, 1);
+            RenderTexture.mainTextureOffset = new Vector2((1 - pixelRatioAdjustment) / 2, 0);
+            WorldCamera.orthographicSize = TargetY / 2;
+        }
+        else
+        {
+            pixelRatioAdjustment = 1f / pixelRatioAdjustment;
+            RenderTexture.mainTextureScale = new Vector2(1, pixelRatioAdjustment);
+            RenderTexture.mainTextureOffset = new Vector2(0, (1 - pixelRatioAdjustment) / 2);
+            WorldCamera.orthographicSize = TargetX / 2;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,21 +5,15 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     public float moveSpeed;
-    public float rotateSpeed;
-    private CharacterController controller;
+
 
 	// Use this for initialization
 	void Start () {
-        controller = GetComponent<CharacterController>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float moveHoriz = Input.GetAxis("Horizontal");
-        float moveVert = Input.GetAxis("Vertical");
-        float moveRotate = Input.GetAxis("Rotate");
-        transform.Rotate(0, moveRotate * rotateSpeed * Time.deltaTime, 0);
-        Vector3 moveVector = transform.TransformDirection(moveHoriz, 0, moveVert);
-        controller.Move(moveVector * moveSpeed * Time.deltaTime);
+
 	}
 }
