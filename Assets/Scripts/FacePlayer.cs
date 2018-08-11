ba6709a6eb939e6640be7966d262fe4966c1bb5a
Rob Hailman <robhailman@gmail.com>
2018-08-10 21:38:43 -0400

    Fix FacePlayer
    don't actually face the player, just match his rotation in 3D space

--- Assets/Scripts/FacePlayer.cs
+++ Assets/Scripts/FacePlayer.cs
@@ -12,6 +12,6 @@ public class FacePlayer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.LookAt(player);
+        transform.rotation = player.rotation;
 	}
 }
